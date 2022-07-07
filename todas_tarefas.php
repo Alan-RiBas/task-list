3cf43a6d8c56801d4c5417b52c6986b22c298cd3
Alan-RiBas <alanbatista.ribas@gmail.com>
2022-07-07 01:41:05 -0300

    imprimindo o status da tarefa

--- todas_tarefas.php
+++ todas_tarefas.php
@@ -45,7 +45,7 @@
 
 								<?php foreach($tarefas as $indice => $tarefa){ ?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9"><?= $tarefa->tarefa ?> </div>
+										<div class="col-sm-9"><?= $tarefa->tarefa ?>(<?= $tarefa->status ?>) </div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
 											<i class="fas fa-edit fa-lg text-info"></i>
